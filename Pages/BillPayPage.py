856f660de0f2f1634437662b410282667c407a08
David Brandao <davfb.ferreira@gmail.com>
2022-06-18 14:44:23 -0300

    Adicionando teste 05

---
+++ Pages/BillPayPage.py
@@ -0,0 +1,72 @@
+import time
+
+from selenium.webdriver.common.by import By
+
+from Pages.General import General
+
+
+class BillPage(General):
+
+    #Url da página
+    url = 'https://parabank.parasoft.com/parabank/billpay.htm'
+
+    #Lateral menu
+    billPayMenuOption = 'Bill Pay'
+
+    #Page title
+    titleCssSelector = 'div h1.title'
+
+    # Form items ID
+    payeeTextBoxName = 'payee.name'
+    addressTextBoxName = 'payee.address.street'
+    cityTextBoxName = 'payee.address.city'
+    stateTextBoxName = 'payee.address.state'
+    zipCodeTextBoxName = 'payee.address.zipCode'
+    phoneTextBoxName = 'payee.phoneNumber'
+    accountTextBoxName = 'payee.accountNumber'
+    verifyAccountTextBoxName = 'verifyAccount'
+    amountTextBoxName = 'amount'
+    sendPaymentButttonCssSelector = 'input[value="Send Payment"]'
+
+    # Complete Screen Data
+    payeeNameID = 'payeeName'
+    amountID = 'amount'
+    fromAccountID = 'fromAccountId'
+
+    titleText = 'Bill Payment Service'
+
+
+    def __init__(self, driver):
+        super(BillPage, self).__init__(driver=driver)
+
+    def open_bill_payment_page(self):
+        # Clicar no item bill pay do menu lateral
+        self.driver.find_element(By.LINK_TEXT, self.billPayMenuOption).click()
+
+        # Verificar URL da página
+        is_url = self.driver.current_url == self.url
+
+        # Verificar o titulo da página
+        element_title = self.driver.find_element(By.CSS_SELECTOR, self.titleCssSelector)
+        is_title = element_title.text == self.titleText
+
+        return is_url and is_title
+
+    def fill_payment_form(self):
+        self.driver.find_element(By.NAME, self.payeeTextBoxName).send_keys('Cesar School')
+        self.driver.find_element(By.NAME, self.addressTextBoxName).send_keys('Marco Zero')
+        self.driver.find_element(By.NAME, self.cityTextBoxName).send_keys('Recife')
+        self.driver.find_element(By.NAME, self.stateTextBoxName).send_keys('PE')
+        self.driver.find_element(By.NAME, self.zipCodeTextBoxName).send_keys('000000')
+        self.driver.find_element(By.NAME, self.phoneTextBoxName).send_keys('000000')
+        self.driver.find_element(By.NAME, self.accountTextBoxName).send_keys('22557')
+        self.driver.find_element(By.NAME, self.verifyAccountTextBoxName).send_keys('22557')
+        self.driver.find_element(By.NAME, self.amountTextBoxName).send_keys('10')
+        self.driver.find_element(By.CSS_SELECTOR, self.sendPaymentButttonCssSelector).click()
+
+    def verify_bill_was_paid(self):
+        time.sleep(1)
+        return (self.driver.find_element(By.ID, self.payeeNameID).is_displayed() and
+                self.driver.find_element(By.ID, self.amountID).is_displayed() and
+                self.driver.find_element(By.ID, self.fromAccountID).is_displayed())
+
---
+++ Tests/test_05.py
@@ -0,0 +1,38 @@
+import time
+
+import pytest
+
+from Pages.BillPayPage import BillPage
+from Pages.LoginPage import LoginPage
+
+
+class Test04:
+    # Test setup (Opens login page)
+    @pytest.fixture()
+    def setup(self, browser):
+        # Login Page Instance
+        self.login_page = LoginPage(browser=browser)
+        # Open Login Page
+        self.login_page.open_login_page()
+        # Logando na aplicação
+        self.login_page.login()
+
+    def teste_pagar_conta(self, setup, tear_down):
+        # Instanciar a classe Bill Pay
+        self.bill_page = BillPage(self.login_page.driver)
+
+        # Acessar e verificar a tela de bill payment
+        assert self.bill_page.open_bill_payment_page()
+
+        # Preencher o pagamento da conta
+        self.bill_page.fill_payment_form()
+
+        # Verificar que o pagamento foi realizado com sucesso
+        assert self.bill_page.verify_bill_was_paid()
+
+
+    # Test TearDown (Closes driver)
+    @pytest.fixture()
+    def tear_down(self):
+        yield
+        self.login_page.QuitDriver()
